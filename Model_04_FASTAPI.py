f3da58c19a294b87bf72a17aa852bcfb9c21c4c5
Gustavo Díaz <gustavo.diaz6990@gmail.com>
2025-10-18 09:59:17 -0300

    asd
    asd

--- Model_04_FASTAPI.py
+++ Model_04_FASTAPI.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 import pickle
 import numpy as np
@@ -12,15 +13,17 @@ with open('scaler.pkl', 'rb') as archivo_scaler:
     scaler = pickle.load(archivo_scaler)
 
 # Definir las características esperadas
-columnas = ['order', 'quotations_prices_estimated', 'quotations_prices_final',
-       'quotation', 'mins_to_register', 'mins_to_quote',
-       'mins_to_reply', 'mins_to_file', 'mins_to_travel', 'totalMins',
-       'marketCode_2H', 'marketCode_2J', 'marketCode_2M', 'marketCode_3N',
-       'marketCode_3O', 'marketCode_3P', 'marketCode_HB', 'typeCode_1',
-       'typeCode_2', 'typeCode_3', 'typeCode_4', 'typeCode_5', 'typeCode_6',
-       'typeCode_7', 'typeCode_8', 'productCode_1', 'productCode_2',
-       'productCode_3', 'productCode_4', 'productCode_5', 'departure_day',
-       'departure_month']
+columnas = [
+    'order', 'quotations_prices_estimated', 'quotations_prices_final',
+    'quotation', 'mins_to_register', 'mins_to_quote', 'mins_to_reply',
+    'mins_to_file', 'mins_to_travel', 'totalMins', 'marketCode_2H',
+    'marketCode_2J', 'marketCode_2M', 'marketCode_3N', 'marketCode_3O',
+    'marketCode_3P', 'marketCode_HB', 'typeCode_1', 'typeCode_2',
+    'typeCode_3', 'typeCode_4', 'typeCode_5', 'typeCode_6', 'typeCode_7',
+    'typeCode_8', 'productCode_1', 'productCode_2', 'productCode_3',
+    'productCode_4', 'productCode_5', 'departure_day', 'departure_month'
+]
+
 # Crear la aplicación FastAPI
 app = FastAPI(title="Determinar si una cotización pueda o no ser aceptada")
 
@@ -65,20 +68,22 @@ async def predecir_cotizacion(transaccion: Transaccion):
     try:
         # Convertir la entrada en un DataFrame
         datos_entrada = pd.DataFrame([transaccion.dict()], columns=columnas)
-        
+
         # Escalar las características
         datos_entrada_scaled = scaler.transform(datos_entrada)
-        
+
         # Realizar la predicción
         prediccion = modelo.predict(datos_entrada_scaled)
         probabilidad = modelo.predict_proba(datos_entrada_scaled)[:, 1]
-        
+
         # Construir la respuesta
         resultado = {
             "Probabilidad de que la cotización SI sea aceptada": bool(prediccion[0]),
             "Probabilidad de que la cotización NO sea aceptada": float(probabilidad[0])
         }
-        
-        return resultado
+
+        # Retornar el resultado directamente en formato JSON
+        return JSONResponse(content=resultado)
+
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
